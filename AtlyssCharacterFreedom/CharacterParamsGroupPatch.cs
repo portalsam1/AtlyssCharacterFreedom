f624993265a077e518a466abf97db2e91cae31cc
portalsam1 <41414314+portalsam1@users.noreply.github.com>
2024-11-26 12:43:33 -0800

    Naming Conventions

--- AtlyssCharacterFreedom/CharacterParamsGroupPatch.cs
+++ AtlyssCharacterFreedom/CharacterParamsGroupPatch.cs
@@ -9,16 +9,16 @@ namespace AtlyssCharacterFreedom
     {
         private static void Postfix(CharacterParamsGroup __instance)
         {
-            __instance._armRange.y = Configuration.armMaximumRange.Value;
-            __instance._bellyRange.y = Configuration.bellyMaximumRange.Value;
-            __instance._boobRange.y = Configuration.boobMaximumRange.Value;
-            __instance._pitchRange.y = Configuration.pitchMaximumRange.Value;
-            __instance._bottomRange.y = Configuration.bottomMaximumRange.Value;
-            __instance._heightRange.y = Configuration.heightMaximumRange.Value;
-            __instance._torsoRange.y = Configuration.torsoMaximumRange.Value;
-            __instance._widthRange.y = Configuration.widthMaximumRange.Value;
-            __instance._headModRange.y = Configuration.headModMaximumRange.Value;
-            __instance._headWidthRange.y = Configuration.headWidthMaximumRange.Value;
+            __instance._armRange.y = Configuration.ArmMaximumRange.Value;
+            __instance._bellyRange.y = Configuration.BellyMaximumRange.Value;
+            __instance._boobRange.y = Configuration.BoobMaximumRange.Value;
+            __instance._pitchRange.y = Configuration.PitchMaximumRange.Value;
+            __instance._bottomRange.y = Configuration.BottomMaximumRange.Value;
+            __instance._heightRange.y = Configuration.HeightMaximumRange.Value;
+            __instance._torsoRange.y = Configuration.TorsoMaximumRange.Value;
+            __instance._widthRange.y = Configuration.WidthMaximumRange.Value;
+            __instance._headModRange.y = Configuration.HeadModMaximumRange.Value;
+            __instance._headWidthRange.y = Configuration.HeadWidthMaximumRange.Value;
         }
     }
 }
--- AtlyssCharacterFreedom/Configuration.cs
+++ AtlyssCharacterFreedom/Configuration.cs
@@ -1,22 +1,20 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using BepInEx.Configuration;
+﻿using BepInEx.Configuration;
 
 namespace AtlyssCharacterFreedom
 {
-    [SuppressMessage("ReSharper", "InconsistentNaming")]
     internal static class Configuration
     {
 
-        public static readonly ConfigEntry<float> armMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "ArmMaximumRange", 105f, "The maximum value that the arm slider will produce in character menus.");
-        public static readonly ConfigEntry<float> bellyMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "BellyMaximumRange", 512f, "The maximum value that the belly slider will produce in character menus.");
-        public static readonly ConfigEntry<float> boobMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "BoobMaximumRange", 512f, "The maximum value that the boob slider will produce in character menus.");
-        public static readonly ConfigEntry<float> pitchMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "PitchMaximumRange", 2f, "The maximum value that the pitch slider will produce in character menus.");
-        public static readonly ConfigEntry<float> bottomMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "BottomMaximumRange", 512f, "The maximum value that the bottom slider will produce in character menus.");
-        public static readonly ConfigEntry<float> heightMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "HeightMaximumRange", 1.055f, "The maximum value that the height slider will produce in character menus.");
-        public static readonly ConfigEntry<float> torsoMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "TorsoMaximumRange", 105f, "The maximum value that the torso slider will produce in character menus.");
-        public static readonly ConfigEntry<float> widthMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "WidthMaximumRange", 1.15f, "The maximum value that the width slider will produce in character menus.");
-        public static readonly ConfigEntry<float> headModMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "HeadModMaximumRange", 100f, "The maximum value that the head modification slider will produce in character menus.");
-        public static readonly ConfigEntry<float> headWidthMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "HeadWidthMaximumRange", 1.15f, "The maximum value that the head width slider will produce in character menus.");
+        public static readonly ConfigEntry<float> ArmMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "ArmMaximumRange", 105f, "The maximum value that the arm slider will produce in character menus.");
+        public static readonly ConfigEntry<float> BellyMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "BellyMaximumRange", 512f, "The maximum value that the belly slider will produce in character menus.");
+        public static readonly ConfigEntry<float> BoobMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "BoobMaximumRange", 512f, "The maximum value that the boob slider will produce in character menus.");
+        public static readonly ConfigEntry<float> PitchMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "PitchMaximumRange", 2f, "The maximum value that the pitch slider will produce in character menus.");
+        public static readonly ConfigEntry<float> BottomMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "BottomMaximumRange", 512f, "The maximum value that the bottom slider will produce in character menus.");
+        public static readonly ConfigEntry<float> HeightMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "HeightMaximumRange", 1.055f, "The maximum value that the height slider will produce in character menus.");
+        public static readonly ConfigEntry<float> TorsoMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "TorsoMaximumRange", 105f, "The maximum value that the torso slider will produce in character menus.");
+        public static readonly ConfigEntry<float> WidthMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "WidthMaximumRange", 1.15f, "The maximum value that the width slider will produce in character menus.");
+        public static readonly ConfigEntry<float> HeadModMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "HeadModMaximumRange", 100f, "The maximum value that the head modification slider will produce in character menus.");
+        public static readonly ConfigEntry<float> HeadWidthMaximumRange = CharacterFreedom.GetConfig().Bind("MaximumValues", "HeadWidthMaximumRange", 1.15f, "The maximum value that the head width slider will produce in character menus.");
         
     }
 }
